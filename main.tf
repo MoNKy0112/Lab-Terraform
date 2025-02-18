a2caeb61bdd324a40de5212e53d94467a9336d50
MoNKy0112 <jomoncaleanoa@unal.edu.co>
2025-02-18 01:17:58 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,48 @@
+
+terraform {
+  required_providers {
+	aws = {
+	  source  = "integrations/github"
+	  version = "~> 6.0.1"
+	}
+  }
+}
+
+variable "github_token" {
+  description = "GitHub personal access token"
+  type        = string
+  sensitive   = true
+}
+
+provider "github" {
+	token = var.github_token
+	owner="MoNKy0112"
+}
+
+
+resource "github_repository" "repository_auto_init" {
+  name        = "RepoTics"
+  description = "Repositorio ArquiTics2024"
+  auto_init   = true
+  visibility  = "public"
+
+  template {
+	owner                = "github"
+	repository = "codespaces-react"
+  }
+}
+
+resource "github_branch" "branch_development" {
+  repository = github_repository.repository_auto_init.name
+  branch     = "development"
+}
+
+resource "github_branch" "branch_qa" {
+  repository = github_repository.repository_auto_init.name
+  branch     = "qa"
+}
+
+resource "github_branch" "branch_production" {
+  repository = github_repository.repository_auto_init.name
+  branch     = "production"
+}
